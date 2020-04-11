ce3d7d9dc25ed9814e27d1a0e7b7265f0fffc506
saitejansvn <nsvn.saiteja@gmail.com>
2020-04-12 00:01:43 +0530

    "final lb"

--- CustomerConsumer/src/main/java/com/app/CustomerConsumerApplication.java
+++ CustomerConsumer/src/main/java/com/app/CustomerConsumerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class CustomerConsumerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hiii");
 		SpringApplication.run(CustomerConsumerApplication.class, args);
 	}
 
